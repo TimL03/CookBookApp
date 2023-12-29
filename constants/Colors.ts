a0de26dd5bd79c3479360c42ff768c923ebf23cd
TimL03 <139620347+TimL03@users.noreply.github.com>
2023-12-29 14:48:15 +0100

    tried to fix colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -10,13 +10,16 @@ const tintColorDark = '#8ED3AE';
 
 export default {
   light: {
-    text: '#fff',
-    background: '#7ea296',
-    mainColorDark: '#55776C',
-    mainColorLight: '#A6CCBD',
-    tint: tintColorDark,
-    tabIconDefault: '#7EA296',
-    tabIconSelected: tintColorDark,
+    text: '#DDE5E8',
+    background: '#274055',
+    mainColorDark: '#1C3041',
+    mainColorLight: '#8499B1',
+    tint: '#5EAF97',
+    tabIconDefault: '#8499B1',
+    tabIconSelected: '#DDE5E8',
+    alert: '#DC544B',
+    alertPressed: '#C1342B',
+    seeThrough: 'rgba(0,0,0,0)',
   },
   dark: {
     text: '#DDE5E8',
